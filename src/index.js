404b5a8ee8441fef05df1940aefc4f526516369b
Shubham Khuntia <64825318+shubhamkhuntia@users.noreply.github.com>
2023-05-16 00:03:28 +0530

    Added basic node express functionalities

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const { ServerConfig, Logger } = require("./config");
+const apiRoutes = require("./routes");
+
+const app = express();
+
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Sucessfully Started server on PORT ${ServerConfig.PORT}`);
+});
